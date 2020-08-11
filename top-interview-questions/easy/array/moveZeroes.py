ebdcf8274aa7ca6b0de1336f7fff4f30fc54b0fe
koni <sgovind.dev@outlook.com>
2020-08-11 21:22:53 +0530

    Create moveZeroes.py

---
+++ top-interview-questions/easy/array/moveZeroes.py
@@ -0,0 +1,14 @@
+#### https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index = 0
+        for i in range(0, len(nums)):
+            if(nums[i] != 0):
+                nums[index] = nums[i]
+                index = index + 1
+
+        for i in range(index, len(nums)):
+            nums[i] = 0
